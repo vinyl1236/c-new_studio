b0d2ad7916646238a0e564d9c732edb6e9246e4f
唱片 <shiyubin1236@gmail.com>
2026-06-23 07:43:12 +0800

    Add prime factorization functionality
    Implement prime factorization for a given number.

---
+++ 15_质因数分解.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int n;
+int main()
+{
+   cin>>n;
+    for(int i=2;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<n/i;
+            break;
+        }
+    }
+    return 0;
+}
